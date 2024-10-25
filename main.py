4776a4d69324fd375855c999cf89ed3af9c2f3a7
dr_spud <xzhuc42@gmail.com>
2024-10-25 21:52:21 +1100

    rotation added

--- main.py
+++ main.py
@@ -11,10 +11,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
     dt = 0
-
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
 
@@ -25,6 +23,7 @@ def main():
                 return
     
         screen.fill(0)
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
 
